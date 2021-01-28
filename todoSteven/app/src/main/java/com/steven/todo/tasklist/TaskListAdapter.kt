6ed37b37b170111dcae067d2ce5afcea446f7c52
Steven Renault <steven.renault@efrei.net>
2021-01-28 11:33:36 +0100

    sûppression d'une tache

--- todoSteven/app/src/main/java/com/steven/todo/tasklist/TaskListAdapter.kt
+++ todoSteven/app/src/main/java/com/steven/todo/tasklist/TaskListAdapter.kt
@@ -4,36 +4,49 @@ import android.icu.text.CaseMap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.steven.todo.R
 
- class TaskListAdapter(private val tasklist: MutableList<Task>) :
+class TaskListAdapter(private val tasklist: MutableList<Task>) :
     RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(taskTitle: String) {
+        fun bind(task: Task, taskTitle: String) {
             itemView.apply {
                 val textViewTask = itemView.findViewById<TextView>(R.id.task_title)
                 textViewTask.text = taskTitle
+
+                val btDelete = itemView.findViewById<ImageButton>(R.id.delete_task)
+                btDelete.setOnClickListener {
+                    System.out.println((task))
+                    onDeleteTask?.invoke(task)
+                }
+
             }
+
         }
     }
 
-     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-         System.out.println(tasklist)
-         val itemView  = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
-        return  TaskViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
 
-     }
+    }
 
-     override fun getItemCount(): Int {
+    override fun getItemCount(): Int {
         return tasklist.size
-     }
+    }
 
-     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+    var onDeleteTask: ((Task) -> Unit)? = null
 
-      holder.bind(tasklist[position].title + "\n" + tasklist[position].description)
-     }
- }
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(tasklist[position], tasklist[position].title + '\n' +tasklist[position].description )
+    }
+
+
+}
--- todoSteven/app/src/main/java/com/steven/todo/tasklist/TaskListFragment.kt
+++ todoSteven/app/src/main/java/com/steven/todo/tasklist/TaskListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,7 @@ class TaskListFragment : Fragment() {
         Task(id = "id_3", title = "Task 3", description = "description 3")
 
     )
+   // private val adapter = (TaskListAdapter(taskList))
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +35,13 @@ class TaskListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        // Pour une [RecyclerView] ayant l'id "recycler_view":
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = (TaskListAdapter(taskList))
+
 // Instanciation d'un objet task avec des données préremplies:
         val newTask: Task =
             Task(
@@ -40,22 +49,18 @@ class TaskListFragment : Fragment() {
                 title = "Task ${taskList.size + 1}",
                 description = "description ${taskList.size + 1}"
             )
-        val bt = view.findViewById<FloatingActionButton>(R.id.add_task)
-        bt.setOnClickListener {
+        val btCreate = view.findViewById<FloatingActionButton>(R.id.add_task)
+        btCreate.setOnClickListener {
 
             taskList.add(newTask)
-            System.out.println(taskList)
-            val adapter :TaskListAdapter = TaskListAdapter(taskList)
-            adapter.notifyDataSetChanged()
+            recyclerView.adapter?.notifyItemChanged(taskList.count() - 1)
         }
 
-        // Pour une [RecyclerView] ayant l'id "recycler_view":
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = (TaskListAdapter(taskList))
+        (recyclerView.adapter as TaskListAdapter).onDeleteTask = { task ->
+            taskList.remove(task)
+            recyclerView.adapter?.notifyItemChanged(taskList.count())
 
+        }
 
     }
-
-
 }
